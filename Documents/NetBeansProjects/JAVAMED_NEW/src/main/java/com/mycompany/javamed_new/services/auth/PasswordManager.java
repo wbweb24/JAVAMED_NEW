7710ee82398cbf2e42c3a3bb055e00c1db1012be
Mila <wbweb23@hotmail.com>
2025-06-07 16:35:10 +0200

    Cambiado a springCrypto

--- Documents/NetBeansProjects/JAVAMED_NEW/src/main/java/com/mycompany/javamed_new/services/auth/PasswordManager.java
+++ Documents/NetBeansProjects/JAVAMED_NEW/src/main/java/com/mycompany/javamed_new/services/auth/PasswordManager.java
@@ -1,16 +1,15 @@
 package com.mycompany.javamed_new.services.auth;
 
-import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordManager {
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    // ✅ Método para generar un hash seguro a partir de una contraseña
     public String hashPassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+        return encoder.encode(password);
     }
 
-    // ✅ Método para verificar si una contraseña es correcta comparándola con su hash
     public boolean checkPassword(String inputPassword, String storedPassword) {
-        return BCrypt.checkpw(inputPassword, storedPassword);
+        return encoder.matches(inputPassword, storedPassword);
     }
 }
